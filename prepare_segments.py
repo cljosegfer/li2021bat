a95beff7406500f08414b24179053557465cf56b
Jose Geraldo Fernandes macaverde604@gmail.com <macaverde604@gmail.com>
2024-06-22 01:07:27 +0000

    add h5 script

--- prepare_segments.py
+++ prepare_segments.py
@@ -25,26 +25,15 @@ label_files = load_label_files(input_directory_label)
 
 num_files = len(label_files)
 # num_files = 5
-# recordings2save = []
-# ratio2save = []
-error = []
-
 n_lead = 12
 n_segment = 10
 beat_length = 400
 
-# h5f = h5py.File('data/challenge2020.h5', 'w')
-# X = h5f.create_dataset('recording', (num_files, n_lead, n_segment, beat_length), dtype='f8')
-# r = h5f.create_dataset('ratio', (num_files, 1, n_segment), dtype='f8')
-
-save_dir = 'data'
-recordings2save = np.memmap(os.path.join(save_dir, 'recordings_' + str(5000) + '_' + str(500) + '_' + str(False) + '.npy'), 
-                            dtype = np.float32, mode = 'w+', shape = (num_files, n_lead, n_segment, beat_length))
-ratio2save = np.memmap(os.path.join(save_dir, 'info_' + str(5000) + '_' + str(500)  + '_' + str(False) + '.npy'), 
-                       dtype = np.float32, mode = 'w+', shape = (num_files, 1, n_segment))
+h5f = h5py.File('data/challenge2020.h5', 'w')
+X = h5f.create_dataset('recording', (num_files, n_lead, n_segment, beat_length), dtype='f8')
+r = h5f.create_dataset('ratio', (num_files, 1, n_segment), dtype='f8')
 
 for i in tqdm(range(num_files)):
-    # print('{}/{}'.format(i + 1, num_files))
     recording, header, name = load_challenge_data(label_files[i], label_dir)
     recording[np.isnan(recording)] = 0
 
@@ -52,37 +41,11 @@ for i in tqdm(range(num_files)):
     recording = resample(recording, header, 500)
 
     # slide and cut
-    try:
-        recording, info2save = slide_and_cut_beat_aligned(recording, 1, 5000, 500,
-                                                      seg_with_r=False, beat_length=400)
-        # X[i, :, :, :] = np.transpose(recording, (0, 2, 1, 3))
-        # r[i, :, :] = info2save
-        recordings2save[i, :, :, :] = np.transpose(recording, (0, 2, 1, 3))
-        ratio2save[i, :, :] = info2save
-    except:
-        print('skipping file: {}, idx: {}'.format(name, i))
-        error.append(name)
-        recordings2save[i, :, :, :] = np.zeros(shape = (1, n_lead, n_segment, beat_length))
-        ratio2save[i, :, :] = np.zeros(shape = (1, 1, n_segment))
-        continue
-    # print(recording)
-    # print(info2save)
-#     recordings2save.append(recording[0])
-#     ratio2save.append(info2save)
-# recordings2save = np.array(recordings2save)
-# recordings2save = np.transpose(recordings2save, (0, 2, 1, 3))
-# ratio2save = np.array(ratio2save)
+    recording, info2save = slide_and_cut_beat_aligned(recording, 1, 5000, 500,
+                                                    seg_with_r=False, beat_length=400)
+    X[i, :, :, :] = np.transpose(recording, (0, 2, 1, 3))
+    r[i, :, :] = info2save
 
-# save_dir = 'data'
-
-# np.save(os.path.join(save_dir, 'recordings_' + str(5000) + '_' + str(
-#                 500) + '_' + str(False) + '.npy'), recordings2save)
-# np.save(os.path.join(save_dir, 'info_' + str(5000) + '_' + str(
-#                 500)  + '_' + str(False) + '.npy'), ratio2save)
-
-# h5f.close()
-
-recordings2save.flush()
-ratio2save.flush()
+h5f.close()
 
 print('done')
